4de84506d926fa6210a774f24ef173d606e5ddf7
Victor Matias <victor.matias@zipline.com.br>
2026-09-18 09:38:21 -0300

    update

--- default.php
+++ default.php
@@ -110,6 +110,15 @@
                 }
             }
         </style>
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-18234177674"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'AW-18234177674');
+        </script>
     </head>
     <body>
         <nav class="navigation">
